637a13a6aeb9f110aed29b10bce6b2f114c57bf9
iceinto <iceinto@163.com>
2017-11-20 18:27:50 +0800

    <工具库><添加><JSON处理>

---
+++ utils/json.go
@@ -0,0 +1,14 @@
+package utils
+
+import "encoding/json"
+
+// 转换 字符串 到 JSON
+func JsonFromStr(str string, v interface{}) error {
+	err := json.Unmarshal([]byte(str), v)
+	return err
+}
+
+func JsonFromByte(byte []byte, v interface{}) error {
+	err := json.Unmarshal(byte, v)
+	return err
+}
