507f052fcc6ffe81272f21c0f03d1edcd149f509
learninto <993232306@qq.com>
2018-04-17 15:09:30 +0800

    增加获取uuid方法

---
+++ utils/uuid/uuid.go
@@ -0,0 +1,29 @@
+// +----------------------------------------------------------------------
+// | 
+// | Created by InteliJ IDE
+// +----------------------------------------------------------------------
+// | Mohoo Go [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.mohoo.com.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | 每位工程师都有保持代码优雅的义务
+// | Each engineer has a duty to keep the code elegant
+// +----------------------------------------------------------------------
+// | @Author: Learn <yanzx@infoepoch.com>
+// | @Date  : 2018/04/2018/4/17
+// | @Time  : 13:57
+// +----------------------------------------------------------------------
+package uuid
+
+import (
+	"github.com/satori/go.uuid"
+	"errors"
+)
+
+func GetUuid() (string, error) {
+	u, e := uuid.NewV4();
+	if e != nil {
+		return "", errors.New("new uuid v4 error")
+	}
+	return u.String(), nil
+}
